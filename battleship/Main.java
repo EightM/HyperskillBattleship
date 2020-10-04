fde44dc54aaa18e24abda6a10f2e2e62ce3da8fe
Виктор Гуков <VAGukov@1cbit.ru>
2020-10-04 15:36:50 +0300

    first stage

---
+++ battleship/Main.java
@@ -0,0 +1,26 @@
+package battleship;
+
+import battleship.ships.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+
+        Ship aircraft = new AircraftCarrier();
+        Ship battleship = new Battleship();
+        Ship submarine = new Submarine();
+        Ship cruiser = new Cruiser();
+        Ship destroyer = new Destroyer();
+
+        game.placeShipOnBoard(aircraft);
+        game.printBoard();
+        game.placeShipOnBoard(battleship);
+        game.printBoard();
+        game.placeShipOnBoard(submarine);
+        game.printBoard();
+        game.placeShipOnBoard(cruiser);
+        game.printBoard();
+        game.placeShipOnBoard(destroyer);
+        game.printBoard();
+    }
+}
